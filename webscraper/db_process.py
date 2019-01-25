aa7974d14a8885d3e7e8e76c0f5eaf9b4e6b3149
David Xie <25449648+DavidXie99@users.noreply.github.com>
2019-01-25 02:43:29 -0500

    code cleanup + bad data handling + improved terminal logging

--- webscraper/db_process.py
+++ webscraper/db_process.py
@@ -14,13 +14,10 @@ def init_conn(db_name):
 def execute():
     global sql_transacts
     for s in sql_transacts:
-        #c.execute(s)
-        #"""
         try:
             c.execute(s)
         except:
             print('Error executing sql:\n',s)
-        #"""
     conn.commit()
     sql_transacts = []
 
@@ -30,21 +27,19 @@ def sql_bldr(sql_string):
         sql_transacts.append(sql_string)
     elif type(sql_string) == list:
         sql_transacts += sql_string
-    if len(sql_transacts) >= 1000:
+    if len(sql_transacts) >= 10000:
         execute()
 
 def obj_parse(data,
               obj_schema,
               to_execute=True):
     query_array = []
-    for table_schema in obj_schema:
-        query_array += table_schema['func'](data,table_schema['sch'])
-        """
-        try:
+    try:
+        for table_schema in obj_schema:
             query_array += table_schema['func'](data,table_schema['sch'])
-        except Exception as e:
-            print('Error occurred generating query: ',e)
-        """
+    except:
+        print('Exception occurred generating SQL for:',data['gameId'])
+        return []
     if to_execute:
         sql_bldr(query_array)
     return query_array
@@ -61,22 +56,24 @@ if __name__ == '__main__':
     import query_builder as qb
     import secrets as s
 
-    
+    num_processed = 0
     init_conn('MatchTest.db')
-    """
+
     files = []
     for r,d,f in walk(s.data_path):
-        files += [f[1]]
-    """
+        files += f
+
     for table_schem in dc.matches_schem:
         sql_bldr(qb.create_query(table_schem))
         
-    for jf in ['2068360217.json']:#files:
-        print(jf)
+    for jf in files:
         with open(s.data_path + jf) as infile:
             data = json.load(infile)
 
             obj_parse(data, dc.matches_schem)
+        num_processed += 1
+        if not num_processed%1000:
+            print('Matches processed',num_processed)
 
     cleanup()
     
